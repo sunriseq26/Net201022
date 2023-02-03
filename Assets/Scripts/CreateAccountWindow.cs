58ec8069e510d6edcd94783bba4743c579cd3069
sunriseq26 <sunriseq26@gmail.com>
2023-02-03 17:19:32 +0300

    End

---
+++ Assets/Scripts/CreateAccountWindow.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class CreateAccountWindow : AccountDataWindowBase
+{
+    [SerializeField] private InputField _mailField;
+    [SerializeField] private Button _createAccountButton;
+
+    private string _mail;
+
+    protected override void SubscriptionsElementsUi()
+    {
+        base.SubscriptionsElementsUi();
+        
+        _mailField.onValueChanged.AddListener(UpdateMail);
+        _createAccountButton.onClick.AddListener(CreateAccount);
+    }
+
+    private void CreateAccount()
+    {
+        ConnectionInfo(true, _connecting, Color.green);
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+        {
+            Username = _username,
+            Email = _mail,
+            Password = _password
+        }, result =>
+        {
+            Debug.Log($"Success: {_username}");
+            ConnectionInfo(false, string.Empty, Color.white);
+            EnterInGameScene();
+        }, error =>
+        {
+            Debug.LogError($"Fail: {error.ErrorMessage}");
+            ConnectionInfo(false, error.ErrorMessage, Color.red);
+        });
+    }
+
+    private void UpdateMail(string mail)
+    {
+        _mail = mail;
+    }
+}
