38752a8906dfe96f9391712f8fae7783aaab8795
sunriseq26 <sunriseq26@gmail.com>
2023-02-15 17:01:06 +0300

    5

--- Assets/Scripts/Game/Authorization/AccountDataWindowBase.cs
+++ Assets/Scripts/Game/Authorization/AccountDataWindowBase.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using Photon.Pun;
 using PlayFab;
 using PlayFab.ClientModels;
 using PlayFab.EconomyModels;
@@ -11,7 +8,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Image = UnityEngine.UI.Image;
 
-public class AccountDataWindowBase : MonoBehaviour
+public class AccountDataWindowBase : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField _usernameField;
     [SerializeField] private InputField _passwordField;
@@ -46,48 +43,9 @@ public class AccountDataWindowBase : MonoBehaviour
     protected void EnterInGameScene(string playFabId)
     {
         SceneManager.LoadScene(1);
-        SetUserData(playFabId);
+        //SetUserData(playFabId);
         //MakePurchase();
-        GetInventory();
-    }
-
-    private void GetInventory()
-    {
-        PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(),
-            result => ShowInvetory(result.Inventory), OnLoginFailure);
-    }
-
-    private void ShowInvetory(List<ItemInstance> inventory)
-    {
-        var firstItem = inventory.First();
-        Debug.Log($"{firstItem.ItemId}");
-        ConsumePotion(firstItem.ItemInstanceId);
-    }
-
-    private void ConsumePotion(string itemInstanceId)
-    {
-        PlayFabClientAPI.ConsumeItem(new ConsumeItemRequest
-        {
-            ConsumeCount = 1,
-            ItemInstanceId = itemInstanceId
-        },result =>
-        {
-            Debug.Log("Complete ConsumePotion");
-        }, OnLoginFailure);
-    }
-
-    private void MakePurchase()
-    {
-        PlayFabClientAPI.PurchaseItem(new PurchaseItemRequest
-        {
-            CatalogVersion = "main",
-            ItemId = "health_potion",
-            Price = 3,
-            VirtualCurrency = "SC"
-        },result =>
-        {
-            Debug.Log("Complete PurchaseItem");
-        }, OnLoginFailure);
+        //GetInventory();
     }
 
     protected void ConnectionInfo(bool enable, string message, Color color)
@@ -95,38 +53,5 @@ public class AccountDataWindowBase : MonoBehaviour
         _bgConnection.enabled = enable;
         _textProcess.text = message;
         _textProcess.color = color;
-    }
-    
-    private void SetUserData(string playFabId)
-    {
-        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest
-        {
-            Data = new Dictionary<string, string>
-            {
-                { "time_recieve_daily_reward", DateTime.UtcNow.ToString() }
-            }
-        }, result =>
-        {
-            Debug.Log("SetUserData");
-            GetUserData(playFabId, "time_recieve_daily_reward");
-        }, OnLoginFailure);
-    }
-
-    private void GetUserData(string playFabId, string keyData)
-    {
-        PlayFabClientAPI.GetUserData(new GetUserDataRequest
-        {
-            PlayFabId =  playFabId
-        }, result =>
-        {
-            if (result.Data.ContainsKey(keyData))
-                Debug.Log($"{keyData}: {result.Data[keyData].Value}");
-        }, OnLoginFailure);
-    }
-
-    private void OnLoginFailure(PlayFabError error)
-    {
-        var errorMessage = error.GenerateErrorReport();
-        Debug.LogError($"Something went wrong: {errorMessage}");
     }
 }
